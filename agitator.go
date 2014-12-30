d3f4358b362e2a486f586129e6036b8680b435a2
Lefteris Zafiris <zaf.000@gmail.com>
2014-12-30 22:58:07 +0000

    Fixed interrupt handling, added some basic structures and logic

--- agitator.go
+++ agitator.go
@@ -21,10 +21,17 @@ import (
 	"net/url"
 	"os"
 	"os/signal"
-	"strings"
+	"strconv"
 	"sync"
 	"sync/atomic"
 )
+
+// Session struct
+type AgiSession struct {
+	clientCon net.Conn
+	serverCon net.Conn
+	request   *url.URL
+}
 
 var (
 	conf   = flag.String("conf", "/etc/agitator.conf", "Configuration file")
@@ -38,13 +45,12 @@ func main() {
 	if *conf == "" {
 		log.Fatal("No configuration file specified.")
 	}
-
+	// Do config stuff...
 
 	// Handle interrupts
 	var shutdown int32
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, os.Interrupt)
-	go handleHangup(sigChan, &shutdown)
 
 	// Create a listener and start a new goroutine for each connection.
 	addr := net.JoinHostPort(*listen, *port)
@@ -55,108 +61,119 @@ func main() {
 	}
 	defer ln.Close()
 	wg := new(sync.WaitGroup)
-	for atomic.LoadInt32(&shutdown) == 0 {
-		conn, err := ln.Accept()
-		if err != nil {
-			log.Println(err)
-			continue
+	go func() {
+		for atomic.LoadInt32(&shutdown) == 0 {
+			conn, err := ln.Accept()
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			if *debug {
+				log.Printf("Connected: %v <-> %v\n", conn.LocalAddr(), conn.RemoteAddr())
+			}
+			wg.Add(1)
+			go connHandle(conn, wg)
 		}
-		if *debug {
-			log.Printf("Connected: %v <-> %v\n", conn.LocalAddr(), conn.RemoteAddr())
-		}
-		wg.Add(1)
-		go connHandle(conn, wg)
-	}
+	}()
+	signal := <-sigChan
+	log.Printf("Received %v, Waiting for remaining sessions to end to exit.\n", signal)
+	atomic.StoreInt32(&shutdown, 1)
 	wg.Wait()
+	return
 }
 
 func connHandle(client net.Conn, wg *sync.WaitGroup) {
+	sess := new(AgiSession)
+	sess.clientCon = client
+	var err error
 	defer func() {
-		client.Close()
+		sess.clientCon.Close()
 		wg.Done()
 	}()
-	// Read the AGI Env variables and get the request string
-	request, env := parseEnv(client)
 
+	// Read the AGI Env variables and parse the request url.
+	env, err := sess.parseEnv()
+	if err != nil {
+		log.Println(err)
+		return
+	}
 	// Do the routing
-	host, err := parseAgiReq(request)
+	err = sess.route()
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
 	// Dial remote server and send the AGI Env data
-	server, err := net.Dial("tcp", host)
+	sess.serverCon, err = net.Dial("tcp", sess.request.Host)
 	if err != nil {
 		log.Println(err)
 		return
 	}
-	defer server.Close()
-	server.Write(env)
+	defer sess.serverCon.Close()
+	env = append(env, []byte("agi_request: "+sess.request.String()+"\n\r\n")...)
+	sess.serverCon.Write(env)
 	// Relay data between the 2 connections.
 	serverDone := make(chan int)
 	clientDone := make(chan int)
 	go func() {
-		io.Copy(server, client)
+		io.Copy(sess.serverCon, sess.clientCon)
 		clientDone <- 1
 	}()
 	go func() {
-		io.Copy(client, server)
+		io.Copy(sess.clientCon, sess.serverCon)
 		serverDone <- 1
 	}()
 
 	select {
 	case <-serverDone:
-		client.Close()
-		close(serverDone)
+		sess.clientCon.Close()
 		<-clientDone
-		close(clientDone)
 	case <-clientDone:
-		server.Close()
-		close(clientDone)
+		sess.serverCon.Close()
 		<-serverDone
-		close(serverDone)
 	}
+	close(serverDone)
+	close(clientDone)
 	return
 }
 
-func parseEnv(c net.Conn) (string, []byte) {
-	var request string
+// Read the AGI environment, return it and parse the agi_request url.
+func (s *AgiSession) parseEnv() ([]byte, error) {
 	agiEnv := make([]byte, 0, 512)
-	buf := bufio.NewReader(c)
+	var req string
+	var err error
+	buf := bufio.NewReader(s.clientCon)
 	for i := 0; i <= 150; i++ {
 		line, err := buf.ReadBytes(10)
 		if err != nil || len(line) <= len("\r\n") {
 			break
 		}
-		agiEnv = append(agiEnv, line...)
-		if request == "" {
-			ind := bytes.IndexByte(line, ':')
-			if string(line[:ind]) == "agi_request" {
-				ind += len(": ")
-				request = string(line[ind : len(line)-1])
-			}
+		ind := bytes.IndexByte(line, ':')
+		if string(line[:ind]) == "agi_request" && len(line) >= ind+len(": \n") {
+			ind += len(": ")
+			req = string(line[ind : len(line)-1])
+		} else {
+			agiEnv = append(agiEnv, line...)
 		}
 	}
-	agiEnv = append(agiEnv, []byte("\r\n")...)
-	return request, agiEnv
+	if req == "" {
+		err = fmt.Errorf("Non valid AGI request")
+	} else {
+		s.request, err = url.Parse(req)
+	}
+	return agiEnv, err
 }
 
-func parseAgiReq(request string) (string, error) {
-	u, err := url.Parse(request)
-	if err != nil {
-		return "", err
+// Shim
+func (s *AgiSession) route() error {
+	srvHost := "127.0.0.1"
+	srvPort := 4545
+	var err error
+	if s.request.Path == "/myagi" {
+		s.request.Host = srvHost + ":" + strconv.Itoa(srvPort)
+	} else {
+		err = fmt.Errorf("No route found")
 	}
-	host := strings.Split(u.Host, ":")
-	if host[0] == "127.0.0.1" && u.Path == "/myagi" {
-		return host[0] + ":4545", nil
-	}
-	return "", fmt.Errorf("Unkown request:%s%s", host[0], u.Path)
-}
-
-// Stop listening for new connections when received an Interrupt signal
-func handleHangup(sch <-chan os.Signal, shutdown *int32) {
-	signal := <-sch
-	log.Printf("Received %v, Waiting for remaining sessions to end to exit.\n", signal)
-	atomic.StoreInt32(shutdown, 1)
+	return err
 }
