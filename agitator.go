d9f051c5479b0907500522655b8df0681c74a808
Lefteris Zafiris <zaf.000@gmail.com>
2015-01-02 16:21:04 +0000

    Added suport for multiple hosts for each destination (only failover mode for now)

--- agitator.go
+++ agitator.go
@@ -30,50 +30,84 @@ import (
 )
 
 const (
-	server = iota
-	client = iota
+	server     = iota
+	client     = iota
+	agiEnvMax  = 150
+	agiEnvSize = 512
+	agiPort    = ":4573"
+	confPath   = "/etc/agitator.conf"
 )
 
 // AgiSession struct
 type AgiSession struct {
-	ClientCon net.Conn
-	ServerCon net.Conn
+	ClientCon *net.TCPConn
+	ServerCon *net.TCPConn
 	Request   *url.URL
 }
 
-// conf file structs
+// RouteTable holds the routing table
+type RouteTable map[string]Path
+
+// Config file struct
 type Config struct {
 	Listen string
 	Port   int
 	Debug  bool
-	App   map[string]Server
+	Route  map[string]App
 }
 
-//
+// App struct
+type App struct {
+	Hosts []string
+	Mode  string
+}
+
+// Path struct
+type Path struct {
+	Hosts []Server
+	Mode  string
+}
+
+// Server struct
 type Server struct {
-	Host string
-	Port int
+	Host  string
+	Count int
 }
 
-var config Config
+var (
+	rtable RouteTable
+	debug  bool
+)
 
 func main() {
-	var conf = flag.String("conf", "/etc/agitator.conf", "Configuration file")
+	var config Config
+	var confFile = flag.String("conf", confPath, "Configuration file")
 	flag.Parse()
-	_, err := toml.DecodeFile(*conf, &config)
+	_, err := toml.DecodeFile(*confFile, &config)
 	if err != nil {
 		log.Fatal(err)
 	}
-	//log.Println(config)
+	debug = config.Debug
+
+	// Generate Routing table
+	rtable = genRtable(config)
+	if len(rtable) == 0 {
+		log.Fatal("No routes specified")
+	}
+
 	// Handle interrupts
 	var shutdown int32
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, os.Interrupt)
 
 	// Create a listener and start a new goroutine for each connection.
-	addr := net.JoinHostPort(config.Listen, strconv.Itoa(config.Port))
-	log.Printf("Starting AGItator proxy on %v\n", addr)
-	ln, err := net.Listen("tcp", addr)
+	addr := config.Listen + ":" + strconv.Itoa(config.Port)
+	tcpaddr, err := net.ResolveTCPAddr("tcp", addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Starting AGItator proxy on %v\n", tcpaddr)
+	ln, err := net.ListenTCP("tcp", tcpaddr)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -81,12 +115,12 @@ func main() {
 	wg := new(sync.WaitGroup)
 	go func() {
 		for atomic.LoadInt32(&shutdown) == 0 {
-			conn, err := ln.Accept()
+			conn, err := ln.AcceptTCP()
 			if err != nil {
 				log.Println(err)
 				continue
 			}
-			if config.Debug {
+			if debug {
 				log.Printf("Connected: %v <-> %v\n", conn.LocalAddr(), conn.RemoteAddr())
 			}
 			wg.Add(1)
@@ -100,7 +134,29 @@ func main() {
 	return
 }
 
-func connHandle(conn net.Conn, wg *sync.WaitGroup) {
+// Generate Routing table from config data
+func genRtable(conf Config) RouteTable {
+	table := make(RouteTable)
+	for path, app := range conf.Route {
+		if len(app.Hosts) == 0 {
+			log.Println("No routes for", path)
+			continue
+		}
+		p := Path{}
+		p.Mode = app.Mode
+		for _, host := range app.Hosts {
+			if strings.Index(host, ":") == -1 {
+				host += agiPort
+			}
+			s := Server{host, 0}
+			p.Hosts = append(p.Hosts, s)
+		}
+		table[path] = p
+	}
+	return table
+}
+
+func connHandle(conn *net.TCPConn, wg *sync.WaitGroup) {
 	sess := new(AgiSession)
 	sess.ClientCon = conn
 	var err error
@@ -121,14 +177,8 @@ func connHandle(conn net.Conn, wg *sync.WaitGroup) {
 		log.Println(err)
 		return
 	}
-
-	// Dial remote server and send the AGI Env data
-	sess.ServerCon, err = net.Dial("tcp", sess.Request.Host)
-	if err != nil {
-		log.Println(err)
-		return
-	}
 	defer sess.ServerCon.Close()
+	// Send the AGI env to the server.
 	env = append(env, []byte("agi_request: "+sess.Request.String()+"\n\r\n")...)
 	sess.ServerCon.Write(env)
 	// Relay data between the 2 connections.
@@ -154,11 +204,11 @@ func connHandle(conn net.Conn, wg *sync.WaitGroup) {
 
 // Read the AGI environment, return it and parse the agi_request url.
 func (s *AgiSession) parseEnv() ([]byte, error) {
-	agiEnv := make([]byte, 0, 512)
+	agiEnv := make([]byte, 0, agiEnvSize)
 	var req string
 	var err error
 	buf := bufio.NewReader(s.ClientCon)
-	for i := 0; i <= 150; i++ {
+	for i := 0; i <= agiEnvMax; i++ {
 		line, err := buf.ReadBytes(10)
 		if err != nil || len(line) <= len("\r\n") {
 			break
@@ -182,13 +232,40 @@ func (s *AgiSession) parseEnv() ([]byte, error) {
 // Route based on reguest path
 func (s *AgiSession) route() error {
 	var err error
+	var tcpaddr *net.TCPAddr
+
 	path := strings.TrimPrefix(s.Request.Path, "/")
-	if server, ok := config.App[path]; ok {
-		s.Request.Host = server.Host + ":" + strconv.Itoa(server.Port)
-	} else if server, ok := config.App["*"]; ok {
-		s.Request.Host = server.Host + ":" + strconv.Itoa(server.Port)
+	if debug {
+		log.Println("Routing request for:", path)
+	}
+	dest, ok := rtable[path]
+	if ok {
+		if debug {
+			log.Println("Found route for", path)
+		}
 	} else {
-		err = fmt.Errorf("No route found")
+		dest, ok = rtable["*"]
+		if !ok {
+			err = fmt.Errorf("No route found")
+			return err
+		}
+		if debug {
+			log.Println("Using wildcard route for", path)
+		}
+	}
+	//if dest.Mode == "failover" || dest.Mode == "" {
+	for _, server := range dest.Hosts {
+		tcpaddr, err = net.ResolveTCPAddr("tcp", server.Host)
+		if err != nil {
+			continue
+		}
+		s.ServerCon, err = net.DialTCP("tcp", nil, tcpaddr)
+		if err == nil {
+			s.Request.Host = server.Host
+			break
+		} else if debug {
+			log.Println("Failed to connect to", tcpaddr, err)
+		}
 	}
 	return err
 }
