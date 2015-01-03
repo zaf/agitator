b8c6234a0011572ad7c0382a6393ab27472f678d
Lefteris Zafiris <zaf.000@gmail.com>
2015-01-03 14:21:29 +0000

    Added connection timeout option, prettified logging

--- agitator.go
+++ agitator.go
@@ -27,6 +27,7 @@ import (
 	"strings"
 	"sync"
 	"sync/atomic"
+	"time"
 
 	"github.com/BurntSushi/toml"
 )
@@ -36,31 +37,34 @@ const (
 	client     = iota
 	agiEnvMax  = 150
 	agiEnvSize = 512
+	wildCard   = "*"
 	agiPort    = ":4573"
 	confPath   = "/etc/agitator.conf"
 )
 
 var (
-	rtable RouteTable
-	climit int
-	debug  bool
+	rtable      RouteTable
+	dialTimeout time.Duration
+	climit      int
+	debug       bool
 )
 
 // AgiSession struct
 type AgiSession struct {
-	ClientCon *net.TCPConn
-	ServerCon *net.TCPConn
+	ClientCon net.Conn
+	ServerCon net.Conn
 	Request   *url.URL
 }
 
 // Config file struct
 type Config struct {
-	Listen string
-	Port   int
-	Conlim int
-	Log    string
-	Debug  bool
-	Route  map[string]struct {
+	Listen  string
+	Port    int
+	Timeout int
+	Conlim  int
+	Log     string
+	Debug   bool
+	Route   map[string]struct {
 		Hosts []string
 		Mode  string
 	}
@@ -120,6 +124,7 @@ func (s PerPriority) Less(i, j int) bool {
 }
 
 func main() {
+	// Parse Config file
 	var config Config
 	var confFile = flag.String("conf", confPath, "Configuration file")
 	flag.Parse()
@@ -133,6 +138,9 @@ func main() {
 			log.SetOutput(logwriter)
 		}
 	}
+
+	// Store Global values
+	dialTimeout = time.Duration(float64(config.Timeout)) * time.Second
 	climit = config.Conlim
 	debug = config.Debug
 
@@ -149,31 +157,29 @@ func main() {
 
 	// Create a listener and start a new goroutine for each connection.
 	addr := config.Listen + ":" + strconv.Itoa(config.Port)
-	tcpaddr, err := net.ResolveTCPAddr("tcp", addr)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Starting AGItator proxy on %v\n", tcpaddr)
-	ln, err := net.ListenTCP("tcp", tcpaddr)
+	log.Printf("Starting AGItator proxy on %v\n", addr)
+	ln, err := net.Listen("tcp", addr)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer ln.Close()
 	wg := new(sync.WaitGroup)
+
 	go func() {
 		for atomic.LoadInt32(&shutdown) == 0 {
-			conn, err := ln.AcceptTCP()
+			conn, err := ln.Accept()
 			if err != nil {
 				log.Println(err)
 				continue
 			}
 			if debug {
-				log.Printf("Connected: %v <-> %v\n", conn.LocalAddr(), conn.RemoteAddr())
+				log.Printf("%v: Connected to %v\n", conn.RemoteAddr(), conn.LocalAddr())
 			}
 			wg.Add(1)
 			go connHandle(conn, wg)
 		}
 	}()
+
 	signal := <-sigChan
 	log.Printf("Received %v, Waiting for remaining sessions to end to exit.\n", signal)
 	atomic.StoreInt32(&shutdown, 1)
@@ -215,7 +221,7 @@ func genRtable(conf Config) RouteTable {
 	return table
 }
 
-func connHandle(conn *net.TCPConn, wg *sync.WaitGroup) {
+func connHandle(conn net.Conn, wg *sync.WaitGroup) {
 	sess := new(AgiSession)
 	sess.ClientCon = conn
 	var err error
@@ -264,10 +270,11 @@ func connHandle(conn *net.TCPConn, wg *sync.WaitGroup) {
 
 // Read the AGI environment, return it and parse the agi_request url.
 func (s *AgiSession) parseEnv() ([]byte, error) {
-	agiEnv := make([]byte, 0, agiEnvSize)
 	var req string
 	var err error
+	agiEnv := make([]byte, 0, agiEnvSize)
 	buf := bufio.NewReader(s.ClientCon)
+
 	for i := 0; i <= agiEnvMax; i++ {
 		line, err := buf.ReadBytes(10)
 		if err != nil || len(line) <= len("\r\n") {
@@ -282,7 +289,7 @@ func (s *AgiSession) parseEnv() ([]byte, error) {
 		}
 	}
 	if req == "" {
-		err = fmt.Errorf("Non valid AGI request")
+		err = fmt.Errorf("%v: Non valid AGI request", s.ClientCon.RemoteAddr())
 	} else {
 		s.Request, err = url.Parse(req)
 	}
@@ -292,42 +299,40 @@ func (s *AgiSession) parseEnv() ([]byte, error) {
 // Route based on reguest path
 func (s *AgiSession) route() error {
 	var err error
-	var tcpaddr *net.TCPAddr
+	client := s.ClientCon.RemoteAddr()
 
 	path := strings.TrimPrefix(s.Request.Path, "/")
 	if debug {
-		log.Println("Routing request for:", path)
+		log.Printf("%v: Request for: %s\n", client, path)
 	}
 	dest, ok := rtable[path]
 	if ok {
 		if debug {
-			log.Println("Found route for", path)
+			log.Printf("%v: Found route for %s\n", client, path)
 		}
 	} else {
-		dest, ok = rtable["*"]
+		dest, ok = rtable[wildCard]
 		if !ok {
-			err = fmt.Errorf("No route found")
+			err = fmt.Errorf("%v: No route found for %s", client, path)
 			return err
 		}
 		if debug {
-			log.Println("Using wildcard route for", path)
+			log.Printf("%v: Using wildcard route for %s\n", client, path)
 		}
 	}
 	hosts := make([]Server, 0, len(dest.Hosts))
 	for srv, pref := range dest.Hosts {
 		pref.RLock()
 		if climit > 0 && pref.Count >= climit {
-			if debug {
-				log.Println("Reached the connections limit in", srv)
-			}
 			pref.RUnlock()
+			log.Printf("%v: Reached connections limit in %s\n", client, srv)
 			continue
 		}
 		hosts = append(hosts, Server{srv, pref.Priority, pref.Count})
 		pref.RUnlock()
 	}
 	if len(hosts) == 0 {
-		return fmt.Errorf("No routes available")
+		return fmt.Errorf("%v: No routes available", client)
 	}
 	if dest.Mode == "balance" {
 		// Load Balance mode: Sort server by number of active sessions
@@ -337,34 +342,28 @@ func (s *AgiSession) route() error {
 		sort.Sort(PerPriority(hosts))
 	}
 	for _, server := range hosts {
-		tcpaddr, err = net.ResolveTCPAddr("tcp", server.Host)
-		if err != nil {
-			continue
-		}
-		s.ServerCon, err = net.DialTCP("tcp", nil, tcpaddr)
+		s.ServerCon, err = net.DialTimeout("tcp", server.Host, dialTimeout)
 		if err == nil {
 			s.Request.Host = server.Host
 			updateCount(path, server.Host, 1)
 			break
 		} else if debug {
-			log.Println("Failed to connect to", tcpaddr, err)
+			log.Printf("%v: Failed to connect to %s\n", client, server.Host)
 		}
 	}
 	return err
 }
 
 // Update active session counter
-func updateCount(path, host string, i int) {
-	dest, ok := rtable[path]
-	if !ok {
-		dest, ok = rtable["*"]
-		if !ok {
-			return
+func updateCount(req, host string, i int) {
+	for _, path := range []string{req, wildCard} {
+		if dest, ok := rtable[path]; ok {
+			if _, ok := dest.Hosts[host]; ok {
+				dest.Hosts[host].Lock()
+				dest.Hosts[host].Count += i
+				dest.Hosts[host].Unlock()
+				return
+			}
 		}
-	}
-	if _, ok = dest.Hosts[host]; ok {
-		dest.Hosts[host].Lock()
-		dest.Hosts[host].Count += i
-		dest.Hosts[host].Unlock()
 	}
 }
